5223cedcd6fead410ce31026116ed2ba4765fe78
nyigaawebsite <90470018+nyigaawebsite@users.noreply.github.com>
2021-09-19 13:41:02 +0300

    Add files via upload

---
+++ Admin.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INVENTORY ADMINISTRATION PAGE</title>
+    <link rel="stylesheet" href="../CSS/admin.css">
+</head>
+
+<div id="heading">
+    
+    <h2>GAMBELLA UNIVERSITY STOCK INENTORY MANAGEMENT SYSTEM</h2>
+
+</div>
+
+
+<div class="navi">
+        <ul>
+            <li><a href="transactview.php">Transaction</a</li>
+            <li><a href="addressview.php">Address</a></li>
+            <li><a href="prodview.php">Product</a></li>
+            <li><a href="itemview.php">Item</a></li>
+            <li><a href="brandview.php">Brand</a></li>
+            <li><a href="orderview.php">Order</a></li>
+            <li><a href="catview.php" >Category</a></li>
+            <li><a href="orderitview.php" >Orderitem</a></li>
+            <li><a href="prodcatview.php" >Productcat</a></li>
+            <li><a href="logout.php" >Logout</a></li>
+       </ul>
+</div>
+
+<body>
+    <div class="main">
+        <hr>
+        <marquee behavior="" direction="left">
+            <h2>WELCOME TO GAMBELLA UNIVERSITY STOCK INVENTORY MANAGEMENT SYSTEM</h2>
+        </marquee>
+        <hr>
+        
+    </div>
+    <div class="foot">
+        
+        <h2>All &copy copyrights are reserved at Gambella University</h2>
+    </div>
+
+</body>
+</html>
